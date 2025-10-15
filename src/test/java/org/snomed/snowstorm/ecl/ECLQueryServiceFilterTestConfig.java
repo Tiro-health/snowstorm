31484ea0fb3c27a2238f61b2d5f422babb1a5ed5
Jaak <jaak.daemen@tiro.health>
2025-10-15 12:13:37 +0000

    Fix ECLQueryServiceFilterTest bean initialization failure
    Remove @TestConfiguration annotation from ECLQueryServiceFilterTestConfig that was
    causing NullPointerException during bean initialization. The annotation changed the
    initialization order, causing @PostConstruct to run before @Autowired fields were
    injected in the parent class.
    
    This fixes 13 test errors in ECLQueryServiceFilterTest.
    
    Root cause: branchService was null when beforeAll() @PostConstruct method tried
    to use it because Spring hadn't injected the @Autowired fields yet.
    
    Solution: Remove unnecessary @TestConfiguration annotation since the class already
    extends ECLQueryTestConfig which extends TestConfig (@SpringBootApplication).
    
    Also added detailed CI/CD failure investigation report documenting all current
    test failures and their root causes.

--- src/test/java/org/snomed/snowstorm/ecl/ECLQueryServiceFilterTestConfig.java
+++ src/test/java/org/snomed/snowstorm/ecl/ECLQueryServiceFilterTestConfig.java
@@ -4,7 +4,6 @@ import org.jetbrains.annotations.NotNull;
 import org.snomed.snowstorm.core.data.domain.*;
 import jakarta.annotation.PostConstruct;
 import org.snomed.snowstorm.core.data.services.ServiceException;
-import org.springframework.boot.test.context.TestConfiguration;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -16,7 +15,6 @@ import static io.kaicode.elasticvc.domain.Branch.MAIN;
 import static org.snomed.snowstorm.TestConcepts.DISORDER;
 import static org.snomed.snowstorm.core.data.domain.Concepts.*;
 
-@TestConfiguration
 public class ECLQueryServiceFilterTestConfig extends ECLQueryTestConfig {
 
 	private static final String DEF_STATUS = "900000000000444006";
